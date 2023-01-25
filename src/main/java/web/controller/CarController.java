aff4601a081d37d4283c216f715b336eb7c029ff
Drujishe <homandru@yandex.ru>
2023-01-25 08:30:17 +0300

    ver.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,22 +3,22 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
 import web.service.CarService;
 
 
 @Controller
+@RequestMapping(value = "/cars")
 public class CarController {
-    private final WebApplicationContext context;
     private final CarService carService;
 
-    public CarController(WebApplicationContext context, CarService carService) {
-        this.context = context;
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
-    @GetMapping(value = "/cars")
+    @GetMapping
     public String carsPage(@RequestParam(value = "count", defaultValue = "-1") int count, Model model) {
         model.addAttribute("list", carService.getCars(count));
         return "/cars";
